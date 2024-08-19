cdbeb9d904bedaaf48682d6fec456088f2286401
tristan_retali <tristan.retali@gmail.com>
2024-08-19 22:21:10 +0200

    package-lock.json

--- travelbook-frontend/src/App.jsx
+++ travelbook-frontend/src/App.jsx
@@ -1,10 +1,19 @@
-import logo from './logo.svg';
 import './App.css';
+import { SignupForm } from './components/ui-implementation/signup-form'
 
 function App() {
   return (
-    <div className="App">
-      <p></p>
+    <div className="App flex flex-row	justify-center items-center min-h-screen font-sans subpixel-antialiased text-white">
+      <div className="min-w-96">
+        <h1 className="text-4xl font-bold pl-52">
+          Welcome<br />
+          to your<br />
+          travel diary
+        </h1>
+      </div>
+      <>
+      <SignupForm/>
+      </>
     </div>
   );
 }
