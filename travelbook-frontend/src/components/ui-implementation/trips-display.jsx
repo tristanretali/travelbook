51e8ced62c13e02c01b07ccae07c1a0a0940e826
tristan_retali <tristan.retali@gmail.com>
2025-02-26 17:55:01 +1300

    [FIX] Change the sow trip route in GET

--- travelbook-frontend/src/components/ui-implementation/trips-display.jsx
+++ travelbook-frontend/src/components/ui-implementation/trips-display.jsx
@@ -1,8 +1,27 @@
 import React from 'react'
 import Card from '../ui/card'
+import axios from 'axios'
 
 export default class TripsDisplay extends React.Component{
+  constructor(props) {
+    super(props);
+    this.getTripsUser = this.getTripsUser.bind(this);
+  }
+
+  getTripsUser(){
+    const url = 'http://localhost:3333/api/trip/show'
+    const headers = {'Authorization': 'Bearer ' + this.props.user.token}
+    axios.get(url, {headers})
+      .then((response) => {
+        console.log(response.data)
+      })
+      .catch((error) => {
+        console.log(error);
+      })
+  }
+
   render() {
+    this.getTripsUser()
     return(
       <>
         <Card
