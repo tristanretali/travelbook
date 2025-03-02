17d85e826400eca18c9f46720472245bb854811f
tristan_retali <tristan.retali@gmail.com>
2025-03-02 15:03:49 +1300

    [FEATURE] Added the parameter tripId when an entry is created

--- travelbook-backend/app/controllers/entries_controller.ts
+++ travelbook-backend/app/controllers/entries_controller.ts
@@ -2,18 +2,20 @@ import { HttpContext } from '@adonisjs/core/http'
 import { creationEntryValidator } from '#validators/entry'
 import EntryService from '#services/entry_service'
 import { inject } from '@adonisjs/core'
+import Trip from '#models/trip'
 
 @inject()
 export default class EntriesController {
   constructor(protected entryService: EntryService) {}
 
   async create({ request, auth }: HttpContext) {
-    const { content } = request.only(['content'])
+    const { content, tripId } = request.only(['content', 'tripId'])
     await auth.authenticate()
 
     if (auth.isAuthenticated) {
       await request.validateUsing(creationEntryValidator)
-      const entry = await this.entryService.createEntry(content)
+      const trip = await Trip.query().where('id', tripId).firstOrFail()
+      const entry = await this.entryService.createEntry(content, trip)
       await entry.save()
 
       return {
--- travelbook-backend/app/services/entry_service.ts
+++ travelbook-backend/app/services/entry_service.ts
@@ -1,9 +1,11 @@
 import Entry from '#models/entry'
+import Trip from '#models/trip'
 
 export default class EntryService {
-  async createEntry(content: string) {
+  async createEntry(content: string, trip: Trip) {
     const entry = new Entry()
     entry.content = content
+    entry.tripId = trip.id
 
     return entry
   }
